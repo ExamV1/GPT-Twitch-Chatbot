120ae7e8ac4a23552291840537df2115e5964819
Exam <87576045+ExamV1@users.noreply.github.com>
2023-03-01 06:10:04 +0000

    Update logic.js

--- logic.js
+++ logic.js
@@ -2,8 +2,10 @@ const electron = require("electron");
 const ipc = electron.ipcRenderer;
 const newConnectToTwitch = require("./connectToTwitch");
 const handleMessage = require("./messageHandlerAIon")
-//const handleMessage = require("./messageHandlerAIoff")
-//use twurple instead
+//const handleMessage = require("./messageHandlerAIoff")  //if you want to use the other messageHandler simply add a comment before it and remove the comment from this one
+
+//if your curious what AIon is it basically makes the bot only respond to messages with the !ai command
+//and AIoff will make the bot respond to all messages detected in the chat
 
 
 document.getElementById("tmilink").addEventListener("click", function () {
